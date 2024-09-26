47e3bb0616b15b5a3419d35a2617d29f7cea5003
Siam-al-islam <siamalislam692@gmail.com>
2024-09-26 21:59:04 +0600

    slider updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Banner from './components/banner/Banner'
 function App() {
 
   return (
-    <>
+    <div className='max-w-7xl mx-auto'>
       <Banner />
-    </>
+    </div>
   )
 }
 
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -15,7 +15,8 @@ const Banner = () => {
                 effect={'coverflow'}
                 grabCursor={true}
                 centeredSlides={true}
-                slidesPerView={'auto'}
+                slidesPerView={3}
+                spaceBetween={-20}
                 coverflowEffect={{
                     rotate: 50,
                     stretch: 0,
@@ -23,12 +24,12 @@ const Banner = () => {
                     modifier: 1,
                     slideShadows: true,
                 }}
-                pagination={true}
+
                 modules={[EffectCoverflow, Pagination]}
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                    <img className='h-full' src="https://scontent.fdac24-5.fna.fbcdn.net/v/t39.30808-6/461337238_1523052049092548_8177598416639902616_n.jpg?stp=dst-jpg_p180x540&_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeG6XmW8xQWyyG_CrvXDcjcbRAD2ln89fltEAPaWfz1-W1nNBqXvaHi3zDH7iyykGkrxjTHjpIxET0gSVqY9A7xj&_nc_ohc=ssipijuOagcQ7kNvgFxOESU&_nc_ht=scontent.fdac24-5.fna&_nc_gid=AMNuc5H_Ee0bd2BwyIIlRZQ&oh=00_AYBA4-pieHV9q8c7P3_vZkhAJ8SCBLi_DoAFDlTUVvrbQA&oe=66FB494A" />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
